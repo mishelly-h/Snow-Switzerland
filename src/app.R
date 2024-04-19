5233162adfa8ccc1fe1a33d0885d52b20dfb610e
Michelle Hunn <mihunn@student.ubc.ca>
2024-04-19 10:00:27 -0700

    first draft of dashboard

--- src/app.R
+++ src/app.R
@@ -1,17 +1,21 @@
 options(shiny.port = 8050, shiny.autoreload = TRUE)
 
 library(shiny)
-# library(ggplot2)
 library(tidyverse)
 library(plotly)
 library(rjson)
 library(dplyr)
+library(glue)
 
 
 df <- read_csv('../data/preprocessed/all_data.csv')
-df <- df[220000:221000, ]
+month_order <- c('July', 'August', 'September', 'October', 
+                 'November', 'December', 'January', 'February', 
+                 'March', 'April', 'Mai', 'June')
 
+df$Month_fct <- factor(df$Month, levels = month_order, ordered=TRUE)
 
+colors <- c(rep('grey', each = 50))
 
 
 ui <- fluidPage(
@@ -23,23 +27,34 @@ ui <- fluidPage(
   sidebarLayout(
     
     sidebarPanel(
-    selectInput(
-      'station_filter',
-      'Select station',
-      choices = unique(df$Name)
-    )
+      selectInput(
+        'station_filter',
+        'Select a Station',
+        choices = unique(df$Name)
+      ),
+      selectInput(
+        'metric_filter',
+        'Select a Metric',
+        choices = colnames(df)[4:6]
+      ),
+      selectInput(
+        'month_filter',
+        'Select a Month',
+        choices = unique(df$Month)
+      ),
+      selectInput(
+        'year_filter',
+        'Select a Year',
+        choices = unique(df$Year)
+      )
     ),
     
     mainPanel(
-      plot_geo(df, locationmode = 'europe') |>
-        layout(geo = list(scope = 'europe'),
-               mapbox = list(
-                 zoom = 5,
-                 center = list(lon = 46, lat = 8))) |>
-        add_markers(x = ~Longitude, y = ~Latitude),
       
-      
-      plotOutput('plot', width = '400px')
+      plotOutput('map_plot', width = '300px'),
+      plotOutput('lineplot', width = '300px'),
+      plotOutput('barplot', width = '300px'),
+      textOutput('selected_var')
       )
     )
   )
@@ -48,37 +63,69 @@ ui <- fluidPage(
 # Server side callbacks/reactivity
 server <- function(input, output, session) {
   
+  # map
+  output$map_plot <- renderPlot({
+    return(
+      df |> 
+      plot_geo(locationmode = 'europe') |>
+        layout(geo = list(scope = 'europe'),
+               mapbox = list(
+                 zoom = 5,
+                 center = list(lon = 46, lat = 8))) |>
+        add_markers(x = ~Longitude, y = ~Latitude) 
+        # add_markers(data = filter(df, Name %in% c(input$station_filter) & 
+        #                             Year %in% c(input$year_filter)), 
+        #             x = ~Longitude, 
+        #             y = ~Latitude,
+        #             color = 'red')
+    )
+  })
+  
   # lineplot per selection
-  output$plot <- renderPlot({
+  output$lineplot <- renderPlot({
     return(
       df |> 
         filter(Name %in% c(input$station_filter)) |> 
-           ggplot(aes(x = month, y = HSmean, color = factor(year))) +
-           geom_line() +
-           labs(title = 'HSmean values over 12 months', 
-                x = 'Month', 
-                y = 'HSmean', 
-                color = 'Year') +
-          scale_color_discrete(name = 'Year') +
-          theme_minimal()
+        ggplot() +
+        geom_line(aes(x = Month_help, 
+                      y = !!sym(input$metric_filter),
+                      color = factor(Year)
+        )) +
+        labs(title = glue('{input$metric_filter} cm of Snow in {input$station_filter}'), 
+             x = 'Month', 
+             y = glue('{input$metric_filter} cm of Snow'), 
+             color = 'Year') +
+        scale_color_manual(values = colors, guide = 'none') +
+        scale_x_continuous(breaks = c(1,2,3,4,5,6,7,8,9,10,11,12),
+                           labels = c("July", "August", "September", "October", "November", 
+                                      "December", "January", "February", "March", "April", 
+                                      "May", "June")) +
+        theme(axis.text.x = element_text(angle=45, vjust=1, hjust=1)) +
+        geom_line(data = filter(df, Name == 'Altdorf' & Year == 2010),
+                  aes(x = Month_help, 
+                      y = !!sym(input$metric_filter)),
+                  color = 'red')
       )
   })
   
-  # test map
-  output$plot_deaths <- renderPlotly({
+  # debug
+  output$selected_var <- renderText({
+    paste("you have selected", input$metric_filter)
+  })
+  
+  # barchart
+  output$barplot <- renderPlot({
     return(
-      ggplotly(
-        ggplot(df,
-               aes(x = year, y = deaths, color = Country)
-        ) +
-          geom_line() +
-          geom_line() +
-          labs(
-            x = "Date",
-            y = "Number of Deaths",
-            title = "Cumulative no. of confirmed, probable, and suspected deaths"
-          )
-      )
+      df |> 
+        filter(Name %in% c(input$station_filter)) |>
+        filter(Month %in% c(input$month_filter)) |> 
+        ggplot(aes(x=!!sym(input$metric_filter))) +
+        geom_histogram(stat="density") +
+        labs(title = glue('{input$metric_filter} cm of Snow in {input$month_filter} in {input$station_filter}'), 
+             x = glue('{input$metric_filter} cm of Snow')
+             #y = 'Count'
+             ) +
+        theme_minimal()
     )
   })
   
@@ -89,36 +136,4 @@ server <- function(input, output, session) {
 
 # Run the app/dashboard
 shinyApp(ui, server)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- src/preprocessing.R
+++ src/preprocessing.R
@@ -1,200 +1,52 @@
 library(tidyverse)
-library(plotly)
-library(rjson)
-library(lubridate)
 
 daily_CH_METEO <- read_csv('../data/raw/data_monthly_CH_METEOSWISS.csv')
-daily_CH_SLF <- read_csv('../data/raw/data_monthly_CH_SLF.csv')
 meta <- read_csv('../data/raw/meta_all.csv')
 
-df <- left_join(daily_CH_METEO, meta, by = "Name") 
+start_year <- 1970
+end_year <- 2015
+
+df <- left_join(daily_CH_METEO, meta, by = "Name") |> 
+  filter(HN_year_start <= start_year, HS_year_start <= start_year) |> 
+  filter(HN_year_end >= end_year, HS_year_end >= end_year) |> 
+  filter(year >= start_year, year <= end_year)
+
+df <- df[!(df$Name %in% unique(df[!complete.cases(df), ]$Name)),]
+
+df <- df |> 
+  mutate(Name = case_when(
+    Name == 'Andermatt_CH_METEOSWISS' ~ 'Andermatt',
+    Name == 'Arosa_CH_METEOSWISS' ~ 'Arosa',
+    Name == 'Elm_CH_METEOSWISS' ~ 'Elm',
+    Name == 'Samedan_CH_METEOSWISS' ~ 'Samedan',
+    TRUE ~ Name
+  ))
+
+df$month <- factor(month.name[df$month], levels = c(
+  "July", "August", "September", "October", "November", "December", 
+  "January", "February", "March", "April", "May", "June"))
+  
+
+df$Month_help <- df$month
+
+df <- df |> 
+  mutate(Month_help = case_when(
+    Month_help == 'July' ~1, 
+    Month_help == 'August' ~2, 
+    Month_help == 'September' ~3, 
+    Month_help == 'October' ~4, 
+    Month_help == 'November' ~5, 
+    Month_help == 'December' ~6, 
+    Month_help == 'January' ~7, 
+    Month_help == 'February' ~8, 
+    Month_help == 'March' ~9, 
+    Month_help == 'April' ~10, 
+    Month_help == 'May' ~11, 
+    Month_help == 'June' ~12
+  )) |> 
+  select(Name, year, month, HNsum, HSmean, HSmax, Longitude, Latitude, Elevation, Month_help)
+
+names(df) <- c("Name", "Year", "Month", "Total", "Average", 
+               "Maximum", "Longitude", "Latitude", "Elevation", "Month_help")
 
 write_csv(df, file = "../data/preprocessed/all_data.csv")
-
-data <- daily_CH_METEO
-year <- 1980
-name <- 'Aadorf_Tanikon'
-
-daily_CH_METEO |> 
-  filter(Name == name) |> 
-  # filter(year == year) |> 
-  ggplot(aes(x=month, 
-             y=HSmean, 
-             color=year)) +
-  geom_line(stat = 'summary', 
-            fun = mean)
-
-
-
-
-p <- plot_geo() |> 
-  add_markers(
-    data = df, x = ~Longitude, y = ~Latitude, alpha = 0.5)|> 
-  layout(
-    geo = list(
-      scope = 'europe',
-      lakecolor = toRGB('white')
-    ),
-    mapbox = list(
-      zoom = 200,
-      center = list(lon = 46, lat = 8)
-    ))
-
-p
-
-
-
-df <- read_csv('../data/preprocessed/all_data.csv')
-df <- df[220000:221000, ]
-
-df |> 
-  filter(Name == 'Zurich_Fluntern') |>
-  ggplot(aes(x = month, y = HSmean, color = factor(year))) +
-  geom_line() +
-  labs(title = 'TEST HSmean values over 12 months', x = 'Month', y = 'HSmean', color = 'Year') +
-  scale_color_discrete(name = 'Year') +
-  theme_minimal()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-options(shiny.port = 8050, shiny.autoreload = TRUE)
-
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(plotly)
-
-data <- read.csv("../data/raw/ebola_2014_2016_clean.csv") |>
-  mutate(
-    cases = Cumulative.no..of.confirmed..probable.and.suspected.cases,
-    deaths = Cumulative.no..of.confirmed..probable.and.suspected.deaths,
-    date = as.Date(Date)
-  )
-
-data$Date <- as.Date(data$Date)
-
-
-# Layout
-ui <- fluidPage(
-  theme = bslib::bs_theme(bootswatch = 'darkly'),
-  
-  titlePanel(
-    h1("2014-2016 Ebola Outbreak in Western Africa"),
-    h2("Exploring cumulative cases and deaths")
-  ),
-  
-  fluidRow(
-    column(3,
-           selectizeInput(
-             "country_multi",
-             "Select country:",
-             choices = unique(data$Country),
-             multi=TRUE,
-             options = list(
-               placeholder = c("Select countries..."),
-               onInitialize = I('function() { this.setValue(["Guinea", "Liberia", "Sierra Leone"]); }'))
-           ),
-           
-           sliderInput(
-             'x_range',
-             'Time period:',
-             min = data$Date |> min(),
-             max = data$Date |> max(),
-             value = c(data$Date |> min(), data$Date |> max())
-           ),
-    ),
-    
-    column(7,
-           plotlyOutput("plot_cases", width = "800px"),
-           plotlyOutput("plot_deaths", width = "800px")
-    )
-    
-  ),
-  fluidRow(
-    column(5,
-           plot_ly(data, 
-                   type='choropleth', 
-                   locationmode = 'country names',
-                   locations=~Country, z=~cases, text=~Country, color=~cases,
-                   colorscale='Purples')
-    ),
-    column(5,
-           plot_ly(data, type='choropleth', locationmode = 'country names',
-                   locations=~Country, z=~deaths, text=~Country, color=~deaths,
-                   colorscale='Reds')
-    )
-  )
-)
-
-# Server side callbacks/reactivity
-server <- function(input, output, session) {
-  
-  filtered_data <- reactive({
-    data |>
-      filter(Country %in% c(input$country_multi)) |>
-      filter(between(date, input$x_range[1], input$x_range[2]))
-    
-  })
-  
-  output$plot_cases <- renderPlotly({
-    return(
-      ggplotly(
-        ggplot(filtered_data(),
-               aes(x = date, y = cases, color = Country)
-        ) +
-          geom_line() +
-          labs(
-            x = "Date",
-            y = "Number of Cases",
-            title = "Cumulative no. of confirmed, probable, and suspected cases"
-          )
-      )
-    )
-  })
-  
-  output$plot_deaths <- renderPlotly({
-    return(
-      ggplotly(
-        ggplot(filtered_data(),
-               aes(x = date, y = deaths, color = Country)
-        ) +
-          geom_line() +
-          geom_line() +
-          labs(
-            x = "Date",
-            y = "Number of Deaths",
-            title = "Cumulative no. of confirmed, probable, and suspected deaths"
-          )
-      )
-    )
-  })
-}
-
-
-
-# Run the app/dashboard
-shinyApp(ui, server)
-
-
-  
-  
-
-
-         
-
-  
